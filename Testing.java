a45b808432916d433a9acdd5c7479c90641bf12e
Kathy <katherine.ll.g2005@gmail.com>
2026-04-02 14:20:11 -0300

    Evidencia colaborativa github-Katherine

--- Testing.java
+++ Testing.java
@@ -1,6 +1,5 @@
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
-        System.out.println("Hola Rama 2");
+        System.out.println("¡Hola Mundo, Soy Katherine Llanquinao!");
     }
 }
